15453dcf82da5f7972eb8e94dee6d2f1fedfe598
mikeditri <mikeditri@hotmail.com>
2019-10-30 21:56:32 -0400

    Added function to convert dtypes and check memory before and after conversion

--- Insurance_Quote_Israel_Mike.py
+++ Insurance_Quote_Israel_Mike.py
@@ -13,9 +13,18 @@ import glob
 
 #pre-processing
 ##type conversions dtype conversion (int64 to int32)
+
+def dtype_conver(Dataframe):
+    for col in Dataframe:
+        if Dataframe[col].dtype == 'float':
+            Dataframe[col] = Dataframe[col].astype(np.float16)
+        if Dataframe[col].dtype == 'int64':
+            Dataframe[col] = Dataframe[col].astype(np.int16)
+
 ##NaN fill with fill with means or medians? Or -1
 ##Data label encoding (SCIKIT LEARN) for call categorical features that are float or number with decimal .
 
+## Check memory usage
 def mem_use(Dataframe):
     mem_use = Dataframe.memory_usage().sum() / 1024 ** 3  # this data set takes >1.7G RAM, we should optimize it after
     print('Memory usage of dataframe is {:.2f} GB'.format(mem_use))
@@ -31,6 +40,9 @@ file_name = os.path.join(DATA_PATH,'train.csv')
 train_df = pd.read_csv(file_name)
 print(train_df.shape)
 
-mem_use(train_df)
 
 #run functions
+
+mem_use(train_df)
+dtype_conver(train_df)
+mem_use(train_df)
